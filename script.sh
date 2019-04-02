b515762d55642c92893ae4a564f9c3f9e15af927
charley <charles.i.saidu@gmail.com>
2019-04-02 16:49:36 +0100

    scripts modification and deleted some unused files

--- script.sh
+++ script.sh
@@ -10,13 +10,13 @@ echo "Starting script"
 # python ./active_deep_seg_max_entropy.py -ds_type 0 -nexp 1
 
 echo "Running script with no dropout"
-python u_net_experiment_bowl_2018.py 0
+python u_net_experiment_bowl_2018.py -ds ./dataset -dt 0
 
 echo "Running script with standard dropout"
-python u_net_experiment_bowl_2018.py 1
+python u_net_experiment_bowl_2018.py -ds ./dataset -dt 1
 
 echo "Running script with max pool dropout"
-python u_net_experiment_bowl_2018.py 2
+python u_net_experiment_bowl_2018.py -ds ./dataset -dt 2
 
 echo "Running script with both standard and maxpool dropout"
-python u_net_experiment_bowl_2018.py 3
+python u_net_experiment_bowl_2018.py -ds ./dataset -dt 3
