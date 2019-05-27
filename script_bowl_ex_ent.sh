4ff619f64ff224c09ace3153480a56a45550803b
charley <charles.i.saidu@gmail.com>
2019-05-27 16:15:02 +0100

    remote rerun 1st 3

--- script_bowl_ex_ent.sh
+++ script_bowl_ex_ent.sh
@@ -4,8 +4,8 @@ python active_u_net_science_bowl_nuclei.py -ds /bigdata/users-data/csatol/charle
 
 python active_u_net_science_bowl_nuclei.py -ds /bigdata/users-data/csatol/charles/datasets/data_science_bowl/ -qt 3 -re_w 0 -mc_p 0 -early_s 1 -reg 1
 
-python active_u_net_science_bowl_nuclei.py -ds /bigdata/users-data/csatol/charles/datasets/data_science_bowl/ -qt 3 -re_w 0 -mc_p 1 -early_s 0 -reg 1
-
-python active_u_net_science_bowl_nuclei.py -ds /bigdata/users-data/csatol/charles/datasets/data_science_bowl/ -qt 3 -re_w 0 -mc_p 1 -early_s 1 -reg 1
-
-python active_u_net_science_bowl_nuclei.py -ds /bigdata/users-data/csatol/charles/datasets/data_science_bowl/ -qt 3 -re_w 1 -mc_p 1 -early_s 1 -reg 1
+# python active_u_net_science_bowl_nuclei.py -ds /bigdata/users-data/csatol/charles/datasets/data_science_bowl/ -qt 3 -re_w 0 -mc_p 1 -early_s 0 -reg 1
+#
+# python active_u_net_science_bowl_nuclei.py -ds /bigdata/users-data/csatol/charles/datasets/data_science_bowl/ -qt 3 -re_w 0 -mc_p 1 -early_s 1 -reg 1
+#
+# python active_u_net_science_bowl_nuclei.py -ds /bigdata/users-data/csatol/charles/datasets/data_science_bowl/ -qt 3 -re_w 1 -mc_p 1 -early_s 1 -reg 1
